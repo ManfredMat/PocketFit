b1094d7a474ef982fca3afd47f5e90a97e0609e9
Stevie <stv8904@hotmail.com>
2021-11-23 13:17:38 -0600

    Modelos event, exercise

--- api/src/models/Exercise.js
+++ api/src/models/Exercise.js
@@ -3,10 +3,31 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define('exercise', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name:{
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+
+    video:{
+      type: DataTypes.STRING,
+      allowNull: true,
+},
+
+    discipline:{
+    type: DataTypes.STRING,
+    allowNull: false,
+}
+
   });
 };
