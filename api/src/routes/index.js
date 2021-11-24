dfb04b69917f34a16cc8d9e010300e8bb5492ebd
Stevie <stv8904@hotmail.com>
2021-11-24 09:14:36 -0600

    Reconfiguracion de routes y controller de exercise

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 
-
 const eventRoute = require('./Routes/Event');
 const exerciseRoute = require('./Routes/Exercise');
 const routineRoute = require('./Routes/Routine');
@@ -8,23 +7,13 @@ const timetableRoute = require('./Routes/Timetable');
 const userRoute = require('./Routes/User');
 
 
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
 
 const router = Router();
-
 
 router.use('/api/event' , eventRoute)
 router.use('/api/exercises' , exerciseRoute)
 router.use('/api/routines' , routineRoute)
 router.use('/api/timetables' , timetableRoute)
 router.use('/api/users' , userRoute)
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
-
 
 module.exports = router;
