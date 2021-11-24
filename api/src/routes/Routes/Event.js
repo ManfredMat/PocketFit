a2f1095950871103dcaba3926dedc50bb17534b7
ManfredMat <87661362+ManfredMat@users.noreply.github.com>
2021-11-24 16:16:26 -0300

    First update Event routes and controllers

--- api/src/routes/Routes/Event.js
+++ api/src/routes/Routes/Event.js
@@ -1,1 +1,16 @@
 const { Router } = require('express');
+const {getAllEvents , getOneEvent , createEvent , updateEventProp ,  removeEvent} = require('../Controllers/Event');
+const router = Router();
+
+router.get('/all',getAllEvents)
+
+router.get('/:id',getOneEvent)
+
+router.post('/',createEvent)
+
+router.put('/:id/:prop' , updateEventProp)
+
+router.delete('/remove/:id', removeEvent)
+
+
+module.exports = router;
