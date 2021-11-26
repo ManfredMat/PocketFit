fc5002b21a7e6ab89c3205904015e88946e2bf6c
Cande Aznarez <candeaznarez00@gmail.com>
2021-11-26 20:33:46 -0300

    SingUp

--- client-native/components/Authentication/SignIn/SignIn.jsx
+++ client-native/components/Authentication/SignIn/SignIn.jsx
@@ -1,61 +1,69 @@
 import React, { useState } from "react";
-import { Button, Text, TextInputComponent, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { Text, TouchableOpacity, View } from "react-native";
 import { Input } from "react-native-elements";
-import ButtonSignIn from '../../../assets/ButtonSignIn'
-import { SvgXml } from 'react-native-svg'
+import ButtonSignIn from "../../../assets/ButtonSignIn";
+import { SvgXml } from "react-native-svg";
 
-const SignIn = ({navigation}) => {
+const SignIn = () => {
+  const navigation = useNavigation();
+  const [state, setState] = useState({ user: "", password: "" });
 
-    const [state, setState] = useState({user:'', password: ''});
+  const handleOnChange = (e, type) => {
+    setState({
+      ...state,
+      [type]: e.nativeEvent.text,
+    });
+    console.log(state);
+  };
 
-    const handleOnChangeUser = (value) => {
-        
-        setState({...state, user: value});
+  const handleOnPress = () => {
+    navigation.navigate("Inicio");
+  };
 
-    }
+  const validateUserAndPass = () => {
+    if (state.user.length < 1 || state.password.length < 1) return true;
+    else return false;
+  };
 
-    const handleOnChangePass = (value) => {
-        
-        setState({...state, password: value});
-        console.log(state);
-
-    }
-
-    const handleOnPress = () => {
-        navigation.navigate("Inicio")
-    }
-
-    const validateUserAndPass = () => {
-        if(state.user.length < 1 || state.password.length < 1) return true;
-        else return false
-    } 
-
-    return(
-
+  return (
     <View>
+      <View>
         <View>
-            <View>
-                <Text>E-mail</Text>
-                <Input inputContainerStyle={{borderBottomWidth:0}} placeholder="Email o Usuario" onChangeText={handleOnChangeUser}/>
-            </View>
+          <Text>E-mail</Text>
+          <Input
+            inputContainerStyle={{ borderBottomWidth: 0 }}
+            placeholder="Email o Usuario"
+            value={state.user}
+            onChange={(e) => handleOnChange(e, "user")}
+          />
+        </View>
 
-            <View>
-                <Text>Contraseña</Text>
-                <Input inputContainerStyle={{borderBottomWidth:0}} name="password" secureTextEntry={true} placeholder="Contraseña" onChangeText={handleOnChangePass}/>
-            </View>
-            </View >
-            <View>
-                <TouchableOpacity disabled={state.user.length < 1 || state.password.length < 1} onPress={() => handleOnPress()}>
-                    <SvgXml xml={ButtonSignIn}/>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <Text>Olvode mi Contraseña</Text>
-            </View>
+        <View>
+          <Text>Contraseña</Text>
+          <Input
+            inputContainerStyle={{ borderBottomWidth: 0 }}
+            name="password"
+            secureTextEntry={true}
+            placeholder="********"
+            value={state.password}
+            onChange={(e) => handleOnChange(e, "password")}
+          />
+        </View>
+      </View>
+      <View>
+        <TouchableOpacity
+          disabled={state.user.length < 1 || state.password.length < 1}
+          onPress={() => handleOnPress()}
+        >
+          <SvgXml xml={ButtonSignIn} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>Olvide mi Contraseña</Text>
+      </View>
     </View>
-
-    )
-
-}
+  );
+};
 
 export default SignIn;
--- client-native/components/Authentication/SignUp/SignUp.jsx
+++ client-native/components/Authentication/SignUp/SignUp.jsx
@@ -1,67 +1,98 @@
-import { useNavigation } from '@react-navigation/core'
-import React, { Component, useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
-import { Input, Button } from 'react-native-elements'
-import ButtonSignUp from '../../../assets/ButtonSignUp'
-import { SvgXml } from 'react-native-svg'
+import { useNavigation } from "@react-navigation/core";
+import React, { Component, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Input, Button } from "react-native-elements";
+import ButtonSignUp from "../../../assets/ButtonSignUp";
+import { SvgXml } from "react-native-svg";
+import postUser from "../../../api/post-login"
+
+export default function SignUp() {
+  const navigation = useNavigation();
+
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+  const [send, setSend] = useState({
+    paymentday:""
+  });
 
 
-export default function SignUp () {
-    const navigation = useNavigation()
-    const [input, setInput] = useState({
-        name: '',
-        email: '',
-        password: '',
-        repeatPassword: ''
-    })
+  React.useEffect(()=>{handleOnSubmit()},[])
 
-    const handleInputChange = (e, type) => {
-        setInput({
-            ...input,
-            [type]: e.nativeEvent.text
-        });
-    }
+  const DateGenerate = () => {
+    const date = new Date();
+    const format = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getUTCDate()}`;
+    return format;
+  };
 
-        return (
-            <View>
-                    <View>
-                        <Text>Nombre</Text>
-                        <Input 
-                            inputContainerStyle={{borderBottomWidth:0}}
-                            placeholder= 'Usuario' 
-                            value={input.name} 
-                            onChange={(e)=>handleInputChange(e, 'name')} 
-                        />
+  const handleInputChange = (e, type) => {
+    setInput({
+      ...input,
+      [type]: e.nativeEvent.text,
+    });
+    console.log(input)
+  };
 
-                        <Text>E-mail</Text>
-                        <Input 
-                            inputContainerStyle={{borderBottomWidth:0}}
-                            placeholder= 'user@example.com' value={input.email} 
-                            onChange={(e)=>handleInputChange(e, 'email')}  
-                        />
+  const handleOnSubmit = async () =>{
+   let datos = {
+    paymentday: DateGenerate(),
+    name: input.name,
+    lastname: "hola",
+    email: input.email,
+    password: input.password,
+  }
 
-                        <Text>Contraseña</Text>
-                        <Input 
-                            inputContainerStyle={{borderBottomWidth:0}}
-                            placeholder= '**********'
-                            value={input.password} 
-                            onChange={(e)=>handleInputChange(e,'password')} 
-                            secureTextEntry={true} 
-                        />
+  const res = await postUser(datos)
 
-                        <Text>Repetir Contraseña</Text>
-                        <Input 
-                            inputContainerStyle={{borderBottomWidth:0}}
-                            placeholder= '**********' 
-                            value={input.repeatPassword} 
-                            onChange={(e)=>handleInputChange(e,'repeatPassword')} 
-                            secureTextEntry={true} 
-                        />
-                    </View>
-                        <TouchableOpacity onPress={()=> navigation.navigate('Inicio')}>
-                            <SvgXml xml={ButtonSignUp}/>
-                        </TouchableOpacity>
-            </View>
-        )
- }
+  console.log(res)
 
+  }
+
+  return (
+    <View>
+      <View>
+        <Text>Nombre</Text>
+        <Input
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          placeholder="Usuario"
+          value={input.name}
+          onChange={(e) => handleInputChange(e, "name")}
+        />
+
+        <Text>E-mail</Text>
+        <Input
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          placeholder="user@example.com"
+          value={input.email}
+          onChange={(e) => handleInputChange(e, "email")}
+        />
+
+        <Text>Contraseña</Text>
+        <Input
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          placeholder="**********"
+          value={input.password}
+          onChange={(e) => handleInputChange(e, "password")}
+          secureTextEntry={true}
+        />
+
+        <Text>Repetir Contraseña</Text>
+        <Input
+          inputContainerStyle={{ borderBottomWidth: 0 }}
+          placeholder="**********"
+          value={input.repeatPassword}
+          onChange={(e) => handleInputChange(e, "repeatPassword")}
+          secureTextEntry={true}
+        />
+      </View>
+      <TouchableOpacity onPress={() => handleOnSubmit()}>
+        <SvgXml xml={ButtonSignUp} />
+      </TouchableOpacity>
+    </View>
+  );
+}
