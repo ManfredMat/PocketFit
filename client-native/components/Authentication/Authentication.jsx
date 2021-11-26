f3a6e4b62cda2733ccd57f8c4e98fae94f10dd8f
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2021-11-26 09:08:44 -0300

    cambios de styles
    backround

--- client-native/components/Authentication/Authentication.jsx
+++ client-native/components/Authentication/Authentication.jsx
@@ -6,7 +6,7 @@ import tw from 'tailwind-react-native-classnames'
 import { SvgXml } from 'react-native-svg'
 import headerLogin from '../../assets/headerLogin'
 import background from '../../assets/Background.png'
-
+import fitnessGym from '../../assets/fitnessGym'
 
 export default function Authentication() {
     
@@ -17,16 +17,19 @@ export default function Authentication() {
             style={{flex: 1, with:'100%', height: '100%'}} 
             imageStyle={{resizeMode: 'stretch'}}>   
 
-            <View style={tw`flex self-center mt-36 mb-5`}>
-            <SvgXml xml={headerLogin}/>
+            <View style={tw`self-center mt-12`}>
+                <SvgXml xml={fitnessGym}/>
+            </View>
+            <View style={tw`flex self-center mt-7 mb-5`}>
+                <SvgXml xml={headerLogin}/>
             </View>
             <View style={tw`bg-gray-300 bg-opacity-40 ml-10 mr-10 rounded-3xl`}>
                 <View style={tw`flex-row self-center mb-5`}>
                     <TouchableOpacity onPress={()=> setRender(false)}>
-                        <Text style={tw`ml-0.5 text-xl`}>Registro</Text>
+                        <Text style={tw`ml-0.5 text-xl text-gray-300`}>Registro</Text>
                     </TouchableOpacity>
                     <TouchableOpacity onPress={()=> setRender(true)}>
-                        <Text style={tw`ml-10 text-xl`}>Iniciar Sesion</Text>
+                        <Text style={tw`ml-10 text-xl text-gray-300`}>Iniciar Sesion</Text>
                     </TouchableOpacity>
                 </View>
                 <View>
