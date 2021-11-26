10dc5c920d031ad911625f4d27c07d6bc2228791
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2021-11-26 12:50:38 -0300

    dead tw

--- client-native/components/Authentication/Authentication.jsx
+++ client-native/components/Authentication/Authentication.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, ImageBackground } from 'react-native'
 import SignUp from './SignUp/SignUp'
 import SignIn from './SignIn/SignIn'
-import tw from 'tailwind-react-native-classnames'
 import { SvgXml } from 'react-native-svg'
 import headerLogin from '../../assets/headerLogin'
 import background from '../../assets/Background.png'
@@ -18,20 +17,20 @@ export default function Authentication() {
             style={{flex: 1, with:'100%', height: '100%'}} 
             imageStyle={{resizeMode: 'stretch'}}>   
 
-            <View style={tw`self-center mt-12`}>
+            <View>
                 <SvgXml xml={fitnessGym}/>
             </View>
-            <View style={tw`flex self-center mt-12 mb-5`}>
+            <View>
                 <SvgXml xml={headerLogin}/>
             </View>
 
-            <View style={tw`bg-gray-300 bg-opacity-40 mt-12 mx-10 rounded-3xl pb-2`}>
-                <View style={tw`flex-row self-center mt-10`}>
+            <View>
+                <View>
                     <TouchableOpacity onPress={()=> setRender(false)}>
-                        <Text style={tw`ml-0.5 text-xl text-gray-300`}>Registro</Text>
+                        <Text>Registro</Text>
                     </TouchableOpacity>
                     <TouchableOpacity onPress={()=> setRender(true)}>
-                        <Text style={tw`ml-10 text-xl text-gray-300`}>Iniciar Sesion</Text>
+                        <Text>Iniciar Sesion</Text>
                     </TouchableOpacity>
                 </View>
                    
@@ -47,7 +46,7 @@ export default function Authentication() {
                     }
                 </View>
             </View>
-            <SvgXml style={tw`self-center absolute mt-56`} xml={userIcon}/>
+            <SvgXml xml={userIcon}/>
         </ImageBackground>
     )
 }
--- client-native/components/Authentication/SignUp/SignUp.jsx
+++ client-native/components/Authentication/SignUp/SignUp.jsx
@@ -2,7 +2,6 @@ import { useNavigation } from '@react-navigation/core'
 import React, { Component, useState } from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 import { Input, Button } from 'react-native-elements'
-import tw from 'tailwind-react-native-classnames'
 import ButtonSignUp from '../../../assets/ButtonSignUp'
 import { SvgXml } from 'react-native-svg'
 
@@ -25,47 +24,41 @@ export default function SignUp () {
 
         return (
             <View>
-                <View style={tw`m-5 content-center h-72`}>
-                    <View style={tw`h-5`} >
-                        <Text style={tw`ml-4 text-gray-300`}>Nombre</Text>
+                    <View>
+                        <Text>Nombre</Text>
                         <Input 
                             inputContainerStyle={{borderBottomWidth:0}}
-                            style={tw`bg-gray-400 bg-opacity-40 pl-3 rounded-3xl text-sm`}
                             placeholder= 'Usuario' 
                             value={input.name} 
                             onChange={(e)=>handleInputChange(e, 'name')} 
                         />
 
-                        <Text style={tw`ml-4 text-gray-300`}>E-mail</Text>
+                        <Text>E-mail</Text>
                         <Input 
                             inputContainerStyle={{borderBottomWidth:0}}
-                            style={tw`bg-gray-400 bg-opacity-40 pl-3 rounded-3xl text-sm`}
                             placeholder= 'user@example.com' value={input.email} 
                             onChange={(e)=>handleInputChange(e, 'email')}  
                         />
 
-                        <Text style={tw`ml-4 text-gray-300`}>Contraseña</Text>
+                        <Text>Contraseña</Text>
                         <Input 
                             inputContainerStyle={{borderBottomWidth:0}}
-                            style={tw`bg-gray-400 bg-opacity-40 pl-3 rounded-3xl text-sm`}
                             placeholder= '**********'
                             value={input.password} 
                             onChange={(e)=>handleInputChange(e,'password')} 
                             secureTextEntry={true} 
                         />
 
-                        <Text style={tw`ml-4 text-gray-300`}>Repetir Contraseña</Text>
+                        <Text>Repetir Contraseña</Text>
                         <Input 
                             inputContainerStyle={{borderBottomWidth:0}}
-                            style={tw`bg-gray-400 bg-opacity-40 pl-3 rounded-3xl text-sm`}
                             placeholder= '**********' 
                             value={input.repeatPassword} 
                             onChange={(e)=>handleInputChange(e,'repeatPassword')} 
                             secureTextEntry={true} 
                         />
                     </View>
-                </View>
-                        <TouchableOpacity style={tw`self-center mb-2`} onPress={()=> navigation.navigate('Inicio')}>
+                        <TouchableOpacity onPress={()=> navigation.navigate('Inicio')}>
                             <SvgXml xml={ButtonSignUp}/>
                         </TouchableOpacity>
             </View>
