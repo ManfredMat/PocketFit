57095e0aa9d0776f4f544dced14f11f250a3b2ba
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2021-11-28 18:01:58 -0300

    landing, login & passreco

--- client/src/components/Login/Login.styles.js
+++ client/src/components/Login/Login.styles.js
@@ -1,30 +1,49 @@
 import styled from "styled-components";
 import imge from '../../assets/img/loginbackground.png'
+
+
 //#FFFFFF4D
 //background-size: contain;
 export const Container = styled.div`
     background-image: url(${imge});
-    background-repeat: no-repeat
-    width: 100%
-    height: 100%
+    background-repeat: no-repeat;
     background-size: cover;
+    background-attachment: fixed;
+    width: 100%;
+    height: 100%;
 `
 
-
-
 export const ContainerIn = styled.div`
-    background-color: #000;
-    border-radius: 3rem;
-    padding: 5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #FFFFFF4D;
+    border-radius: 2rem;
+    padding: 1rem;
     width: 20rem;
-    height: 5rem;
+    height: 12rem;
+    color: #fff;
 `
 export const Input = styled.input`
     background-color: #FFFFFF4D;
-    width: 20rem;
-    padding: .4rem;
-    margin: 1rem
+    width: 15rem;
+    padding: .3rem;
+    margin: 1rem;
     border: none;
     border-radius: 1rem;
     align-self: center;
+`
+export const Btn = styled.button`
+    background-color: var(--green);
+    border-radius: 1rem;
+    border: none;
+    padding: .6rem;
+    width: 10rem;
+    margin-top: 1rem;
+`
+export const TextGreen = styled.h6`
+    color: var(--green);
+`
+export const Wave = styled.img`
+    width: 100%;
 `
