83643f746fc62cdeff8bc9c1d50f75afbfaa4a07
ManfredMat <87661362+ManfredMat@users.noreply.github.com>
2021-11-29 15:49:29 -0300

    agrego bulk de ejercicios , cmabio modelo routine y block

--- api/index.js
+++ api/index.js
@@ -1,20 +1,43 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const {User} = require("./src/db")
+const {User , Exercise} = require("./src/db")
 
+const generatorEx = (n)=>{
+  let array=[]
+  let descripciones=["tirar una pesa y atraparla con la cara","sufrir como condenado" , "tirarse a llorar" , "correr hasta desmayarse"]
+  let disciplina=["Levantamiento" , "Running" ,"Tortura china" , "Lo que nadie quiere hacer"]
+  for(i=0 ; i < n ; i++){
+    let description = descripciones[parseInt(Math.random()*4)]
+    let discipline  = disciplina[parseInt(Math.random()*4)]
+    array.push(
+      {
+        name:`ejercicio n ${[i+1]}`,
+        description,
+        video:"un url",
+        discipline,
+      }
+    )
 
-conn.sync({ alter: true }).then(async () => {
-  
-  let adminAcount = {
-    name:"Admin",
-    lastname:"Fake Gym",
-    email:"soyElAdmin@FakeGym.com",
-    password:"fakegym1234",
-    isadmin:true,
-    isprofessor:false,
-    isuser:false
   }
+  return array
+}
+
+const exerArray = generatorEx(10)
+
+let adminAcount = {
+  name:"Admin",
+  lastname:"Fake Gym",
+  email:"soyElAdmin@FakeGym.com",
+  password:"fakegym1234",
+  isadmin:true,
+  isprofessor:false,
+  isuser:false
+}
+
+conn.sync({ force: true }).then(async () => {
+  
   await User.create(adminAcount)
+  await Exercise.bulkCreate(exerArray)
   server.listen(3001, () => {
     console.log("%s listening at 3001");
   });
