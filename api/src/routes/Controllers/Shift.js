660a321a0e8f3b8a33cf4d03354b864126b0702f
Vchouhy <vchouhy@gmail.com>
2021-11-30 13:21:36 -0300

    changes
    cambios en timetables y creacion de tabla de shift con todo lo que involucra

---
+++ api/src/routes/Controllers/Shift.js
@@ -0,0 +1,58 @@
+const {Shift} = require('../../models/Shift')
+
+const createShift= async (req , res) =>{
+    const { kindOfShift,
+            availability,
+            capacity,
+            beginning,
+            ending,
+            weekday,
+            month,
+            year} = req.body
+    try{
+        const newShift = await Shift.create({ kindOfShift, availability, capacity, beginning, ending, weekday, month, year});
+        res.json(newShift)
+    }
+    catch(err){
+        res.send(err)
+    }}
+
+    
+
+const getAllShifts = async (req , res) =>{
+    try{
+        const allTimetables = await Shift.findAll()
+        res.json(allTimetables)
+    }
+    catch(err){
+        res.send(err)
+    }
+}
+
+const updateShift = async (req , res) =>{
+    const{id, prop} = req.params
+    const {update} = req.body
+try{
+    let oneShift = await Shift.findOne({where:{id:id}})
+    oneShift[prop]= update
+    console.log(oneShift)
+    await oneShift.save()
+    res.send(oneShift)
+}
+catch(err){
+    res.send(err)
+}
+}
+
+const deleteShift = async (req , res) =>{
+    const {id} = req.params
+    try{
+        await Shift.destroy({where:{id:id}})
+        res.send({message: "Shift successfully deleted"})
+    }
+    catch(err){
+        res.send(err)
+    }
+    }
+
+module.exports = { getAllShifts, createShift, updateShift, deleteShift };
--- api/src/routes/Routes/Timetable.js
+++ api/src/routes/Routes/Timetable.js
@@ -1,12 +1,10 @@
 const { Router } = require('express');
-const { createTimetable, getAllTimetables, getTimetableById, updateTimetable, deleteTimetable} = require("../Controllers/Timetable.js")
+const { createTimetable, getTimetableById, updateTimetable, deleteTimetable} = require("../Controllers/Timetable.js")
 
 const router = Router();
 
 
 router.post("/", createTimetable);
-
-router.get("/", getAllTimetables);
 
 router.get("/:id", getTimetableById);
 
