31b1203f1812f266f972f179ec1ce149e7fbe5c0
Stevie <stv8904@hotmail.com>
2021-11-29 19:21:09 -0600

    Controller de login añdido

--- api/src/routes/Routes/Login.js
+++ api/src/routes/Routes/Login.js
@@ -1,19 +1,7 @@
 const { Router } = require("express");
-const passport = require("passport");
 const router = Router();
+const { login } = require("../Controllers/Login.js");
 
-router.post("/", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) return next(err);
-    if (!user) {
-      return res.status(401).json(info.message);
-    } else {
-      req.login(user, function (err) {
-        if (err) return next(err);
-        return res.send(req.session);
-      });
-    }
-  })(req, res, next);
-});
+router.post("/", login);
 
 module.exports = router;
