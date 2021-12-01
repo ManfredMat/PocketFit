91d4230fab8629836b3cb3f1800d263c85392e8d
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2021-12-01 03:35:59 -0300

    c

--- client-native/redux/Actions/actions-Training.js
+++ client-native/redux/Actions/actions-Training.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
+
+export const WEEK_PLAN = "WEEK_PLAN";
 // lean 192.168.1.109:3001
-export const EXERCISE = "EXERCISE";
-  
-export function getAllExercises() {
-  return async function(dispatch) {
-      await axios.get('http://192.168.1.109:3001/api/exercises/')
-      .then(data => {
-        dispatch({
-          type: EXERCISE,
-          payload: data.data
-        })
-      });
+export const getAllWeekPlan = () => async (dispatch) => {
+  try {
+    const res = await axios.get('http://192.168.1.109:3001/api/weekplan/c4b6b320-054e-417c-bffe-aa85905407ba')
+    dispatch({
+      type: WEEK_PLAN,
+      payload: res.data
+    })
+  } catch (error) {
+    console.log(error)
   }
 }
