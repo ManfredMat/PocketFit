4187a441e8303933ece2e1e681ff525d05383cee
Leandro Rebequi <71858342+rebequileandro1398@users.noreply.github.com>
2021-12-06 11:13:55 -0300

    p

--- client-native/redux/Actions/actions-Shifts.js
+++ client-native/redux/Actions/actions-Shifts.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import IP from '../../components/Ips'
 
 export const ALL_SHIFTS = "ALL_SHIFTS";
 export const GET_BY_ID = 'GET_BY_ID';
@@ -14,7 +14,7 @@ export const getAllShifts = (day, month, year) => {
   }
     return async function(dispatch) {
     try {
-      const res = await axios.get('http://192.168.1.109:3001/api/shift/all', { params:query })
+      const res = await axios.get(`http://${IP}:3001/api/shift/all`, { params:query })
       dispatch({
         type: ALL_SHIFTS,
         payload: res.data
@@ -29,7 +29,7 @@ export const getAllShifts = (day, month, year) => {
 export const getShiftId = (id) => {
     return async function(dispatch) {
     try {
-      const res = await axios.get(`http://192.168.1.109:3001/api/shift/${id}`)
+      const res = await axios.get(`http://${IP}:3001/api/shift/${id}`)
       dispatch({
         type: GET_BY_ID,
         payload: res.data
