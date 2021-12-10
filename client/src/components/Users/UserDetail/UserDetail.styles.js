72fd5287cffc54dcd61fce9be30f52441c8e75c8
Luciano Heitt <lheitt@gmail.com>
2021-12-10 13:44:01 -0300

    users update

---
+++ client/src/components/Users/UserDetail/UserDetail.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Styles = {
+    Container: styled.div`
+        width:100vw;
+        height: 100vh;
+        background-color: var(--darkBlue);
+    `,
+}
+
+export default Styles;
