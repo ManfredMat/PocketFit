81f344828ccb9377ceabddd9fc1053dc91269b42
Cande Aznarez <candeaznarez00@gmail.com>
2021-12-15 13:38:19 -0300

    update

--- client/src/components/Home/Styles/HomeStyled.js
+++ client/src/components/Home/Styles/HomeStyled.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components";
+import Styles from "../../Timetable/Styles/CalendarStyled";
 
 const Styles = {
   GlobalStyle: createGlobalStyle`
@@ -11,9 +12,10 @@ const Styles = {
     display: column;
     flex-direction: row;
     width: 100vw;
-    height: 100vh;
     background-color: var(--darkGray-base);
     font-size: ${(props) => (1.65 * props.screenHeight) / 100}px;
+    height: ${(props) => props.overFlow ? "100vh" : "auto"};
+    overflow-y: ${(props) => props.overFlow ? "hidden" : "auto"};
     &::-webkit-scrollbar {
       width: 0.8em;
     }
@@ -30,8 +32,18 @@ const Styles = {
   `,
   StyledFirstColumn: styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     width: 70%;
+  `,
+  RowOne: styled.div`
+      display: flex;
+    flex-direction: row;
+  `,
+  RowTwo: styled.div`
+      display: flex;
+    flex-direction: column;
+    margin-right: 1em;
+    //height: 40em;
   `,
   ContentDiv: styled.div`
     display: flex;
@@ -94,6 +106,64 @@ const Styles = {
     flex-direction: column;
     height: 60%;
   `,
+  WeekPlan: styled.div`
+    flex: 1;
+    display: grid;
+    grid-template-columns: 6% repeat(6, 1fr);
+    grid-template-rows: 8%  92%;
+    gap: 0.6em;
+  `,
+  WeekPlanContainer: styled.div`
+    display: flex;
+  `,
+  FirstRowWeek: styled.div`
+    padding: 0.8em;
+    border-radius: 0.5em;
+    background-color: var(--yellow);
+    color: var(--darkGray-base);
+  `,
+  BlocksGenContainer: styled.div`
+  `,
+  BlocksContainers: styled.div`
+    background-color: ${props => props.block === 1
+        ? '#D9FB52'
+        : props.block === 2
+            ? '#CEFA1F'
+            : props.block === 3
+                ? '#B4E005'
+                : '#D9FB52'
+    };
+    border-radius: 0.5em;
+    margin-bottom: 0.6em;
+    padding: 0.3em 0.5em;
+    height: 10.5em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+  `,
+  exeName: styled.p`
+    background-color: var(--darkGray-base);
+    border-radius: 0.4em;
+    padding: 0.4em;
+    font-size: 0.8em;
+    margin-bottom: 0.5em;
+    text-align: center;
+    text-transform: uppercase;
+  `,
+  BlockName: styled.h3`
+    color: var(--darkGray-base);
+    text-transform: capitalize;
+    font-weight: 500;
+  `,
+  BlockRounds: styled.p`
+    background-color: var(--green);
+    color: var(--darkGray-base);
+    border-radius: 0.6em;
+    text-align: center;
+  `,
+  Nums: Styles.p`
+  `
 };
 
 export default Styles;
