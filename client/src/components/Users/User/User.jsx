1e0c5d134a526c600a839c88914bcb4ccb372723
Luciano Heitt <lheitt@gmail.com>
2021-12-15 06:48:15 -0300

    warnings deleted and native app.json updated

--- client/src/components/Users/User/User.jsx
+++ client/src/components/Users/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Styles from './User.styles';
 import { useDispatch } from 'react-redux';
 import { getUserDetail, renderUserDetail } from '../../../redux/Actions/actions-users';
